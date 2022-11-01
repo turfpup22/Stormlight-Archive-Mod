653099a4005655263f4ec144cc8a3fd3f97cdb1f
vali <tomsmith.d@outlook.com>
2022-11-01 16:10:43 -0400

    Initial

---
+++ src/main/java/net/vali/stormlight/StormlightMod.java
@@ -0,0 +1,46 @@
+package net.vali.stormlight;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vali.stormlight.item.ModItems;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(StormlightMod.MOD_ID)
+public class StormlightMod {
+        // Directly reference a slf4j logger
+        public static final String MOD_ID = "stormlight";
+        private static final Logger LOGGER = LogUtils.getLogger();
+
+    public StormlightMod()
+        {
+            IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+            ModItems.register(eventBus);
+            eventBus.addListener(this::setup);
+
+
+            // Register ourselves for server and other game events we are interested in
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+        private void setup ( final FMLCommonSetupEvent event)
+        {
+            // some preinit code
+            LOGGER.info("HELLO FROM PREINIT");
+            LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        }
+}
---
+++ src/main/java/net/vali/stormlight/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.vali.stormlight.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.vali.stormlight.StormlightMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, StormlightMod.MOD_ID);
+
+    public static final RegistryObject<Item> Amethyst = ITEMS.register("amethyst",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
