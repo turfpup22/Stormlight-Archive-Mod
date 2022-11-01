fe44e6b0b81be8201232ff9019eb6feb4a9ce944
vali <tomsmith.d@outlook.com>
2022-11-01 19:18:15 -0400

    Initial

---
+++ src/main/java/net/vali/stormlight/block/ModBlocks.java
@@ -0,0 +1,4 @@
+package net.vali.stormlight.block;
+
+public class ModBlocks {
+}
--- src/main/java/net/vali/stormlight/item/ModItems.java
+++ src/main/java/net/vali/stormlight/item/ModItems.java
@@ -12,7 +12,19 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, StormlightMod.MOD_ID);
 
-    public static final RegistryObject<Item> Amethyst = ITEMS.register("amethyst",
+    public static final RegistryObject<Item> Sapphire = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Ruby = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Garnet = ITEMS.register("garnet",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Heliodor = ITEMS.register("heliodor",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Topaz = ITEMS.register("topaz",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Zircon = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> Smokestone = ITEMS.register("smokestone",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
